8b7aa2dc5e949bdbc60000cbaefe36fe756f5ea6
tiannian <dtiannian@gmail.com>
2022-03-21 23:11:49 +0800

    add clone

--- karma-p2p-webrtc/src/addr.rs
+++ karma-p2p-webrtc/src/addr.rs
@@ -5,7 +5,7 @@ use webrtc::{
 
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub enum WebrtcAddr {
     #[serde(skip)]
     Bootstrap(Vec<RTCIceServer>),
